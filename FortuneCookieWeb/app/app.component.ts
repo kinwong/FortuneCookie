ec35f53b7778f315d84c1e5c41803e3fe73a38c6
kinwong <kinhangwong@gmail.com>
2017-01-18 01:03:26 +0800

    Doco

--- FortuneCookieWeb/app/app.component.ts
+++ FortuneCookieWeb/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+/**
+ * This is the main component.
+ * 
+ * @export
+ * @class AppComponent
+ */
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`
--- FortuneCookieWeb/app/app.module.ts
+++ FortuneCookieWeb/app/app.module.ts
@@ -2,6 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+/**
+ * Represents the main application module.
+ * 
+ * @export
+ * @class AppModule
+ */
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
